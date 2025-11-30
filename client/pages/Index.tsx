830d47affcd9b6c55e59f9fae664bf9e6026e916
Vo Son Long <sonlong2302@gmail.com>
2025-11-30 17:33:16 +0700

    fix: unreload page when click 'Xem tat ca'

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/site/Hero";
 import ProductCard from "@/components/site/ProductCard";
 import { products as specialProducts } from "@/data/specialProducts";
 import { Flower2 } from "lucide-react";
+import { NavLink } from "react-router-dom";
 
 export default function Index() {
   return (
@@ -15,12 +16,12 @@ export default function Index() {
           <h2 className="text-2xl md:text-3xl font-bold tracking-tight">
             Mặt hàng nổi bật
           </h2>
-          <a
-            href="/shop"
+          <NavLink
+            to="/shop"
             className="text-primary text-sm md:text-base hover:underline"
           >
             Xem tất cả
-          </a>
+          </NavLink>
         </div>
         <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
           {specialProducts.slice(0, 6).map((p) => (
